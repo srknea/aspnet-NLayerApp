b13d0fa9ea71630d2fe94a737539c0b9fb6b960b
Serkan IŞIK <dehnas79@gmail.com>
2023-08-03 15:40:16 +0300

    Create Entities

---
+++ NLayerApp.Core/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        //Bir kategorinin birden fazla ürünü olabilir.
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ NLayerApp.Core/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } 
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+
+        //Bir ürünün bir kategorisi olabilir.
+        public int CategoryId { get; set; } //foreign key
+        public Category Category { get; set; }
+
+        public ProductFeature productFeature { get; set; }
+    }
+}
