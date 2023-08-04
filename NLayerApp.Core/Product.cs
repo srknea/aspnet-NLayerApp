62e5db9a70245bb8be1d609056510b3b73d6586e
Serkan IŞIK <dehnas79@gmail.com>
2023-08-04 10:01:50 +0300

    Changed variables to nullable

--- NLayerApp.Core/Product.cs
+++ NLayerApp.Core/Product.cs
@@ -8,15 +8,24 @@ namespace NLayerApp.Core
 {
     public class Product : BaseEntity
     {
-        public string Name { get; set; } 
+        /*
+        public Product(string name, Category category, ProductFeature productFeature)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Category = category ?? throw new ArgumentNullException(nameof(category));
+            ProductFeature = productFeature ?? throw new ArgumentNullException(nameof(productFeature));
+        }
+        */
+
+        public string? Name { get; set; } 
         public int Stock { get; set; }
         public decimal Price { get; set; }
 
 
         //Bir ürünün bir kategorisi olabilir.
         public int CategoryId { get; set; } //foreign key
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
-        public ProductFeature productFeature { get; set; }
+        public ProductFeature? ProductFeature { get; set; }
     }
 }
