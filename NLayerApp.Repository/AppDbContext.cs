0de51d2571f79fc23623c06e1277997be8db9366
Serkan IŞIK <dehnas79@gmail.com>
2023-08-05 18:30:36 +0300

    Update AppDbContext for Configurations

--- NLayerApp.Repository/AppDbContext.cs
+++ NLayerApp.Repository/AppDbContext.cs
@@ -3,6 +3,7 @@ using NLayerApp.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,22 @@ namespace NLayerApp.Repository
         {
             //Fluent API 
             //modelBuilder.Entity<Category>().HasKey(x => x.Id);
+
+
+            /* Configuration'ları tek tek eklemek */
+            /*
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductFeatureConfiguration());
+            */
+
+            /* Configuration'ları tek satırda eklemek */
+            // IEntityTypeConfiguration interace'ini implemente eden class'ları bulur ve onları bulur ve onları uygular.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            // Assembly nedir? : Solution'daki bütün Class Library'leri temsil eder.
+            // Assembly içerisindeki tüm class'ları bul ve onları uygula.
+            // Neredeki Assembly ? -> Assembly.GetExecutingAssembly() : Bu class'ın bulunduğu assembly
+
             base.OnModelCreating(modelBuilder);
         }
     }
