24ecb9666b502473a5f019e213a71164406d34fe
Serkan IŞIK <dehnas79@gmail.com>
2023-08-05 18:27:19 +0300

    Create ProductFeatureConfiguration

---
+++ NLayerApp.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Repository.Configurations
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId); //Bir ProductFeature'ın bir Product'ı olacak. Bir Product'ın bir ProductFeature'ı olacak. 
+            //(ProductFeature class'ında zaten ayarladık default olarak EF Core bu satırdaki işi zaten yapacak...)    
+        }
+    }
+}
