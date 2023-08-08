e61d014086b7b1e7cd39b7b3f41afb70014a6cc7
Serkan IŞIK <dehnas79@gmail.com>
2023-08-08 12:43:40 +0300

    Fixed an issue related to AutoMapper

--- NLayerApp.API/Program.cs
+++ NLayerApp.API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
 
-builder.Services.AddScoped(typeof(MapProfile));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 builder.Services.AddDbContext<AppDbContext>( x =>
 {
