f11b498d7a384c8fd9d11ccff8203e471c160538
Serkan IŞIK <dehnas79@gmail.com>
2023-08-17 12:34:59 +0300

    Added save method to ProductsController.

--- NLayerApp.Web/Controllers/ProductsController.cs
+++ NLayerApp.Web/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayerApp.Core.DTOs;
+using NLayerApp.Core.Model;
 using NLayerApp.Core.Services;
 
 namespace NLayerApp.Web.Controllers
@@ -6,15 +10,43 @@ namespace NLayerApp.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _productService.GetProductWithCategory());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.Categories = new SelectList(categoriesDto,"Id","Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+                //return RedirectToAction("Index");
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.Categories = new SelectList(categoriesDto,"Id", "Name");
+
+            return View();
         }
     }
 }
