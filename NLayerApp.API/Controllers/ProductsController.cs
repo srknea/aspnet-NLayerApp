99a5948050c359489191829f38bab372bf4aaf95
Serkan IŞIK <dehnas79@gmail.com>
2023-08-18 10:25:47 +0300

    Add - Existing Project - NLayerApp.API

---
+++ NLayerApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerApp.API.Filters;
+using NLayerApp.Core.DTOs;
+using NLayerApp.Core.Model;
+using NLayerApp.Core.Services;
+
+namespace NLayerApp.API.Controllers
+{
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductService _productService;
+        public ProductsController(IMapper mapper, IProductService productService)
+        {
+            _mapper = mapper;
+            _productService = productService;
+        }
+
+        // GET : api/products
+        [HttpGet]
+        public async Task<IActionResult> All() {
+            var products = await _productService.GetAllAsync();
+            var productsDto = _mapper.Map<List<ProductDto>>(products.ToList());
+
+            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDto));
+        }
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))]
+        // GET : api/products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id) {
+
+            var product = await _productService.GetByIdAsync(id);
+
+            var productDto = _mapper.Map<ProductDto>(product);
+
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var product = await _productService.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDto = _mapper.Map<ProductDto>(product);
+
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productsDto));
+            // 201 : Oluşturuldu anlamında kullanılır. İşlem başarılı ise 201 döndürülebilir.
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productUpdateDto)
+        {
+            await _productService.UpdateAsync(_mapper.Map<Product>(productUpdateDto));
+            
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        // DELETE : api/products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            
+            await _productService.RemoveAsync(product);
+
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        // GET : api/products/GetProductsWithCategory
+        //[HttpGet("GetProductsWithCategory")]
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProductsWithCategory() {
+            return CreateActionResult(await _productService.GetProductWithCategory());
+        }
+    }
+}
---
+++ NLayerApp.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using NLayerApp.Core.DTOs;
+using NLayerApp.Service.Exceptions;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace NLayerApp.API.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        NotFoundException => 404,
+                        _ => 500
+                    };
+                    // 500 ile ilgili durum şuan best practice değildir.
+
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomResponseDto<NoContentDto>.Fail(statusCode,exceptionFeature.Error.Message);
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                });
+            });
+        }
+    }
+}
